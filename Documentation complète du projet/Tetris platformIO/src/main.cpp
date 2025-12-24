4377df63d14a106188db1c587026402c37c5a254
A-Letourneau <aleletourneau123@gmail.com>
2025-12-24 14:48:54 -0500

    Création de la fork et placement des fichiers déjà fait

---
+++ Documentation complète du projet/Tetris platformIO/src/main.cpp
@@ -0,0 +1,352 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_IS31FL3731.h>
+
+#define nbRow 16 + 3
+#define nbCol 9
+
+#define LIGHT_LEVEL 3
+
+#define EMPTY 0
+#define SQUARE 1
+#define MOVING_BLOCK 2
+#define START_LAYER 15
+
+#define BUTTON_LEF_PIN 15
+#define BUTTON_MID_PIN 4
+#define BUTTON_RIG_PIN 19
+
+#define NB_OF_SIZE_1_BLOCK 1
+#define NB_OF_SIZE_2_BLOCK 7
+#define NB_OF_SIZE_3_BLOCK 6
+#define NB_OF_SIZE_4_BLOCK 0
+
+#define LINE_CLEAR_DELAY 100 //*3*3
+
+
+
+// If you're using the full breakout...
+Adafruit_IS31FL3731 ledmatrix = Adafruit_IS31FL3731();
+// If you're using the FeatherWing version
+//Adafruit_IS31FL3731_Wing ledmatrix = Adafruit_IS31FL3731_Wing();
+
+
+uint8_t board[nbRow][nbCol] = {{}};
+
+uint32_t speed = 250;
+uint32_t startTime = 0;
+
+uint8_t score = 0;
+
+uint8_t xBlock = 0;
+uint8_t yBlock = 0;
+
+uint8_t sizeOfBlock = 0;
+
+void affichage(){
+  uint8_t light = 0;
+  // animate over all the pixels, and set the brightness from the sweep table
+  for (uint8_t x = 0; x < nbRow - 3; x++)
+    for (uint8_t y = 0; y < nbCol; y++)
+    {
+      if (board[x][y] != 0)
+        light = LIGHT_LEVEL;
+      else
+        light = 0;
+
+      if (x >= 8)
+        ledmatrix.drawPixel(-x + 23, -y + 8, light);
+      else
+        ledmatrix.drawPixel(x, y, light);
+    }
+}
+
+void gameOver(){
+  for (uint8_t y = 0; y < nbCol; y++)
+    if (board[START_LAYER][y] == 1)
+      for (uint8_t x = 0; x < nbRow; x++)
+        for (uint8_t y = 0; y < nbCol; y++)
+          board[x][y] = 0;
+}
+
+void stopBlock(){
+    for (uint8_t x = 0; x < nbRow; x++)
+      for (uint8_t y = 0; y < nbCol; y++)
+        if (board[x][y] == 2)
+          board[x][y] = 1;
+  }
+
+bool checkMovingBlock(){
+  int isBlock = false;
+  for (uint8_t x = 0; x < nbRow; x++)
+    for (uint8_t y = 0; y < nbCol; y++)
+      if (board[x][y] == MOVING_BLOCK)
+        isBlock = true;
+
+  return isBlock;
+}
+
+void createBlock(){
+  const uint8_t size_2_Blocks[NB_OF_SIZE_2_BLOCK][3][2] = {
+      {   
+        { 0, 0 }, //Square
+        { 2, 2 },
+        { 2, 2 } 
+      },
+      {   
+        { 2, 0 }, //T 
+        { 2, 2 },
+        { 2, 0 } 
+      },
+      {   
+        { 0, 2 }, //T mirror
+        { 2, 2 },
+        { 0, 2 } 
+      },
+      {   
+        { 2, 0 }, //L
+        { 2, 0 },
+        { 2, 2 } 
+      },
+      {   
+        { 0, 2 }, //L mirror
+        { 0, 2 },
+        { 2, 2 } 
+      },
+      {   
+        { 0, 2 }, //S
+        { 2, 2 },
+        { 2, 0 } 
+      },
+      {   
+        { 2, 0 }, //Z
+        { 2, 2 },
+        { 0, 2 } 
+
+      }
+    };
+
+  const uint8_t size_3_Blocks[NB_OF_SIZE_3_BLOCK][2][3] = {
+      {
+        { 0, 2, 0 }, //T
+        { 2, 2, 2 } 
+      },
+      {
+        { 2, 2, 2 }, //T mirror
+        { 0, 2, 0 } 
+      },
+      {
+        { 0, 2, 2 }, //Z
+        { 2, 2, 0 } 
+      },
+      {
+        { 2, 2, 0 }, //S
+        { 0, 2, 2 } 
+      },
+      {
+        { 0, 0, 2 }, //L
+        { 2, 2, 2 } 
+      },
+      {
+        { 2, 0, 0 }, //L mirroir
+        { 2, 2, 2 } 
+      }
+    };
+
+  uint8_t typeOfBlock = random(1, NB_OF_SIZE_1_BLOCK + NB_OF_SIZE_2_BLOCK + NB_OF_SIZE_3_BLOCK + NB_OF_SIZE_4_BLOCK );
+  uint8_t posYSpawn = 0;
+  uint8_t randoBlock = 0;
+
+  if (typeOfBlock == 1)
+  {
+    Serial.println("Spawn line ");
+    posYSpawn = random(0, nbCol);
+    for (uint8_t x = START_LAYER; x < nbRow; x++)
+      board[x][posYSpawn] = MOVING_BLOCK;
+    sizeOfBlock = 1;
+  }
+  else if(typeOfBlock > 1 && typeOfBlock < NB_OF_SIZE_1_BLOCK + NB_OF_SIZE_2_BLOCK)
+  {
+    sizeOfBlock = 2;
+    Serial.println("Spawn size 2 blocks ");
+    posYSpawn = random(0, nbCol - 1);
+    randoBlock = random(0, NB_OF_SIZE_2_BLOCK);
+    for (uint8_t x = START_LAYER; x < START_LAYER + 3; x++)
+      for (uint8_t y = posYSpawn; y < sizeOfBlock + posYSpawn; y++)
+        board[x][y] = size_2_Blocks[randoBlock][x - START_LAYER][y - posYSpawn];
+  }
+  else if(typeOfBlock > NB_OF_SIZE_1_BLOCK + NB_OF_SIZE_2_BLOCK && typeOfBlock < NB_OF_SIZE_1_BLOCK + NB_OF_SIZE_2_BLOCK + NB_OF_SIZE_3_BLOCK)
+  {
+    sizeOfBlock = 3;
+    Serial.println("Spawn size 3 blocks ");
+    posYSpawn = random(0, nbCol - 2);
+    randoBlock = random(0, NB_OF_SIZE_3_BLOCK);
+    for (uint8_t x = START_LAYER; x < START_LAYER + 2; x++)
+      for (uint8_t y = posYSpawn; y < sizeOfBlock + posYSpawn; y++)
+        board[x][y] = size_3_Blocks[randoBlock][x - START_LAYER][y - posYSpawn];
+
+  }
+
+    xBlock = START_LAYER;
+    yBlock = posYSpawn;
+}
+
+void makeBlockDown(){
+  for (uint8_t x = 0; x < nbRow; x++)
+  {
+    for (uint8_t y = 0; y < nbCol; y++)
+    {
+      if (board[x][y] == MOVING_BLOCK)
+      {
+        board[x-1][y] = MOVING_BLOCK; 
+        board[x][y] = EMPTY;
+      }
+    }
+  }
+  xBlock -= 1;
+}
+
+bool checkBlockDown(){
+  for (uint8_t x = 0; x < nbRow; x++)
+  {
+    for (uint8_t y = 0; y < nbCol; y++)
+    {
+      if (board[x][y] == MOVING_BLOCK)
+      {
+        if (x > 0)
+        {
+          if (board[x-1][y] == SQUARE)
+            return false;
+        }
+        else
+          return false;
+      }
+    }
+  }
+  return true;
+}
+
+bool checkBlockLorR(char dir){
+  if (dir == 'L')
+  {
+    if (yBlock - 1 < 0)
+    {
+      Serial.println("Cant move, on the left edge");
+      return false;
+    }
+    for(uint8_t x = 0; x < nbRow; x++)
+      {
+        if (board[x][yBlock - 1] == SQUARE && board[x][yBlock] == MOVING_BLOCK)
+        {
+          Serial.println("Cant move, block in the way");
+          return false;
+        }
+      }
+  }
+  return true;
+}
+
+void moveBlockLorR(char dir){
+
+  if (dir == 'L')
+  {
+  for (uint8_t x = 0; x < nbRow; x++)
+    for (uint8_t y = 0; y < nbCol; y++)
+        if (board[x][y] == MOVING_BLOCK)
+        {
+          board[x][y-1] = MOVING_BLOCK;
+          board[x][y] = EMPTY;          
+        }
+    yBlock -= 1;
+  }
+}
+
+void deleteLine(){
+  uint8_t nbSquare = 0;
+  for (uint8_t x = 0; x < nbRow; x++)
+  {
+    nbSquare = 0;
+    for (uint8_t y = 0; y < nbCol; y++)
+      if (board[x][y] == SQUARE)
+        nbSquare += 1;
+
+    if (nbSquare == nbCol)
+    {
+      for (uint8_t i = 0; i < 3; i++)
+      {
+        for (uint8_t y2 = 0; y2 < nbCol; y2++)
+          board[x][y2] = EMPTY;
+        affichage();
+        delay(LINE_CLEAR_DELAY);
+        for (uint8_t y2 = 0; y2 < nbCol; y2++)
+          board[x][y2] = SQUARE;
+        affichage();
+        delay(LINE_CLEAR_DELAY);
+        for (uint8_t y2 = 0; y2 < nbCol; y2++)
+          board[x][y2] = EMPTY;
+        affichage();
+        delay(LINE_CLEAR_DELAY);
+      }
+      score += 1; 
+      for (uint8_t x2 = x + 1; x2 < nbRow; x2++)
+        for (uint8_t y2 = 0; y2 < nbCol; y2++)
+          board[x2 - 1][y2 - 1] = board[x2][y2];
+    }
+  }
+}
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  if (! ledmatrix.begin()) {
+    Serial.println("IS31 not found");
+    while (1);
+  }
+  Serial.println("IS31 found!");
+
+  for (uint8_t x = 0; x < nbRow; x++)
+    for (uint8_t y = 0; y < nbCol; y++)
+      board[x][y] = 0;
+
+  pinMode(BUTTON_LEF_PIN, INPUT);
+  //pinMode(BUTTON_MID_PIN, INPUT);
+  //pinMode(BUTTON_RIG_PIN, INPUT);
+  
+  startTime = millis();
+
+}
+
+void loop() 
+{    
+  affichage();
+
+  deleteLine();
+  if (!checkMovingBlock())
+    createBlock();
+
+  if (checkBlockDown())
+    makeBlockDown();
+  else
+    stopBlock();
+
+  gameOver();
+
+  while((millis() - startTime) < speed)
+  {
+    if (!digitalRead(BUTTON_LEF_PIN))
+    {
+      if(checkBlockLorR('L'))
+      {      
+        moveBlockLorR('L');
+        affichage(); 
+        delay(50);
+      }
+
+    }
+  }
+  Serial.print("Pos x (rows) : "); Serial.print(xBlock);  Serial.print(" Pos y (col) : "); Serial.println(yBlock);
+  startTime = millis();
+}
+
+
--- README.md
+++ README.md
@@ -4,79 +4,8 @@ Concours de Conception de Circuits Imprimés – Université de Sherbrooke, Auto
 
 ## Thème : Carte professionnelle (Business Card)
 
-Bienvenue à la compétition de conception de PCB de l’Université de Sherbrooke, édition automne 2025 !
-Cette année, le défi est de transformer la carte professionnelle traditionnelle en un **objet électronique unique, créatif et fonctionnel**.
-Votre mission : concevoir un PCB au format carte de visite qui intègre des fonctionnalités innovantes, esthétiques et utiles dans un contexte professionnel réel.
+## Contenu
 
----
-
-## Déroulement
-
-<img width="1642" height="631" alt="Capture d’écran du 2025-09-03 15-49-03" src="https://github.com/user-attachments/assets/12521976-ed44-4f69-b275-b95bb3e103db" />
-
-* **25 août** : Lancement de la compétition et ouverture des inscriptions
-* **3 octobre** : Date limite d’inscription
-* **10 octobre** : Date limite de sélection d’idée (formulaire fourni sur GitHub et Discord)
-* **25 août au 31 décembre 23h59** : Période de conception et développement
-* **Dates de commande des PCB et pièces** :
-
-  * 19 septembre\* (LCSC)
-  * 3 octobre\* (LCSC)
-  * 17 octobre\* (LCSC)
-  * 31 octobre\* (LCSC)
-  * 14 novembre
-  * 28 novembre
-  * 12 décembre\*\* (commande finale, réception non garantie avant la fin)
-* **Décembre** : Finalisation des projets
-* **31 décembre 23h59** : Remise finale (schémas, Gerber, BOM, photos, doc)
-* **5 janvier 2026** : Journée de l’événement et présentations publiques
-
----
-
-### Répertoire "Template"  
-Le répertoire "Template" contient tous les documents à remplir pour participer à la compétition. Vous y trouverez :  
-- Formulaire d'idée (à remplir)
-- Formulaire d'inscription (à remplir)
-- Condition et termes de l'inscription
-- TEMPLATE de PowerPoint
-- TEMPLATE pour la documentation
-
----
-
-### Répertoires code, elec et mech  
-Chacun de ces répertoires contient de la documentation de départ pour vous guider. Consultez la section "doc". Ces répertoires sont également là pour vous aider à vous organiser lors du développement de votre règle.
-
----
-
-### Comment forker le dépôt
-
-1. Accédez au dépôt GitHub officiel à l'adresse suivante : `https://github.com/CCI-udes/H25_Pomodoro`.
-2. Cliquez sur le bouton "Fork" en haut à droite de la page du dépôt.
-3. Sélectionnez votre propre compte GitHub comme destination du fork.
-4. Clonez le dépôt forké sur votre machine locale en utilisant la commande suivante :
-   ```bash
-   git clone https://github.com/<votre-utilisateur>/E25_Wearable.git
-   ```
-
----
-
-## Règlement
-
-* Équipes de **1 à 3 personnes**
-* Frais de participation : **10 \$ / membre** OU **courriel de commandite** (gabarits fournis)
-* Dimensions maximales PCB : **88,9 mm x 50,8 mm**
-* BOM maximum : **50 \$**
-* Coût de développement total : **125 \$**
-* Utilisation de **DevBoard permise** (pénalité de points si dans la version finale)
-* Tous les types de composants acceptés (Bluetooth, NFC, OLED, etc.)
-* Batterie rechargeable permise si formation suivie et protocole respecté
-* Objectif : **s’amuser et apprendre** !
-
----
-
-## Remise
-
-Chaque équipe doit fournir via son fork GitHub :
 
 * Schéma électrique (PDF)
 * Fichiers Gerber du PCB + projet EDA OU PDF coloré des couches
@@ -86,42 +15,10 @@ Chaque équipe doit fournir via son fork GitHub :
 
 ---
 
-## Évaluation
-
-Les projets seront notés sur un total de **250 points**, selon :
-
-* **Idée \[15 pts]** (juges + vote des participants)
-* **Fonctionnalité \[35 pts]** (implémentation et compréhension)
-* **Complexité \[45 pts]** (hardware + software)
-* **Gestion des ressources \[25 pts]** (BOM + commandes)
-* **Démonstration \[55 pts]** (oral, écrit, esthétique)
-* **Conception électronique \[75 pts]** (choix composants, schéma, PCB, assemblage, soudures)
-
----
-
-## Prix
-
-* 🏆 **Prix de conception** – Total 250 pts
-* ⚡ **Prix du gningnégnieur** – Total 170 pts (créativité et apprentissage valorisés)
-* 💼 **Prix du vendeur** – Total 125 pts (gestion, organisation, finition)
-
----
-
-## Ressources utiles
-
-* **Microcontrôleurs recommandés** : ESP32-C3, ESP32-S3, RP2040, RP2350, ch32v003
-* **Logiciels de conception** : KiCad, Altium, EasyEDA (+ tutoriels fournis dans le doc)
-* **Idées de business cards électroniques** : [Hackaday](https://hackaday.com/tag/pcb-business-card/), [Reddit](https://www.reddit.com/r/PCB/comments/14jk8cz/pcb_business_card_ideas/), [Exemples NFC](https://www.instructables.com/PCB-Business-Card-With-NFC/)
-* **Discord** : [Lien d’invitation](https://discord.gg/xeSqf3N3Hw)
-
----
-
 ## Contacts
 
-* Jacob Turcotte – Président – `Jacob.Turcotte@usherbrooke.ca`
-* Miriam Caisse – Vice-présidente – `Miriam.Caisse@usherbrooke.ca`
-* Jordan Gagnon – Trésorier – `Jordan.Gagnon@usherbrooke.ca`
-* Céleste-Lyi Martel – Marketing – `Celeste-lyi.martel@usherbrooke.ca`
+* Alexis Létourneau – Participant – `leta0145@usherbrooke.ca`
+
 
 
 
